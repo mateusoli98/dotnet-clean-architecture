2dd1788c18021032c2b150e8c53c7a96c1c8e176
Mateus Oliveira <mateusoli.viana@gmail.com>
2024-05-26 21:55:39 -0300

    Persistence

---
+++ CleanArchitecture/CleanArchitecture.Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Persistence.Context;
+
+namespace CleanArchitecture.Persistence.Repositories
+{
+    public class UnitOfWork(AppDbContext context) : IUnitOfWork
+    {
+        private readonly AppDbContext _context = context;
+
+        public async Task Commit(CancellationToken cancellationToken)
+        {
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ CleanArchitecture/CleanArchitecture.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Persistence.Repositories
+{
+    public class UserRepository(AppDbContext context) : BaseRepository<User>(context), IUserRepository
+    {
+        public async Task<User?> GetByEmail(string email, CancellationToken cancellationToken)
+        {
+            return await _context.Users.FirstOrDefaultAsync(x => x.Email == email, cancellationToken);
+        }
+    }
+}
